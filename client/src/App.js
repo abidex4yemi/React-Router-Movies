e0efc50d679c31f5219e3fea35b49be08b1e7b04
abidex4yemi <abidex4yemi@gmail.com>
2019-06-10 11:32:37 +0100

    Set up router and route for the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
+const routesDetails = [
+	{
+		path: '/',
+		ComponentToRender: MovieList
+	},
+	{
+		path: '/movies/:id',
+		ComponentToRender: Movie
+	}
+];
+
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			savedList: []
+		};
+	}
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
+	addToSavedList = movie => {
+		const savedList = this.state.savedList;
+		savedList.push(movie);
+		this.setState({ savedList });
+	};
 
-  render() {
-    return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<SavedList list={this.state.savedList} />
+					{routesDetails.map(({ path, ComponentToRender }) => {
+						return <Route exact path={path} component={ComponentToRender} />;
+					})}
+				</div>
+			</Router>
+		);
+	}
 }
