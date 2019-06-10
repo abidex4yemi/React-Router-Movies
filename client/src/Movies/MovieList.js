ac54710e6e1936a0d3de1ba2b35e04fa5f89a724
abidex4yemi <abidex4yemi@gmail.com>
2019-06-10 11:48:28 +0100

    Refactor MovieDetails to have its own component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,54 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { MovieDetails } from './MovieDetails';
 
 export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: []
+		};
+	}
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/api/movies')
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
-  }
+	componentDidMount() {
+		axios
+			.get('http://localhost:5000/api/movies')
+			.then(response => {
+				this.setState(() => ({ movies: response.data }));
+			})
+			.catch(error => {
+				return error;
+			});
+	}
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
-}
+	render() {
+		const { movies } = this.state;
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+		return <div className="movie-list">{movies.map(movie => <MovieDetails key={movie.id} movie={movie} />)}</div>;
+	}
 }
